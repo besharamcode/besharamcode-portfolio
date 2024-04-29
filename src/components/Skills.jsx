d2f6831794c48855d6f66ed089eb18cff0389b29
Mohit kushwah <besharamcode@gmail.com>
2024-04-29 10:32:08 +0530

    changes commite

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,7 +7,7 @@ const Skills = () => {
     <motion.div
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
-      viewport={{ amount: 0.5 }}
+      viewport={{ amount: 0.3 }}
       id="skills"
       className="md:px-[6vw] sm:px-[4vw] px-[1rem] h-[100dvh] relative overflow-hidden grid place-items-center"
     >
