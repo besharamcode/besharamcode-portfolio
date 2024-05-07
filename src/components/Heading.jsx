eb578ef50a05b96dd9d92a159b31bb08f7b559c5
Mohit kushwah <besharamcode@gmail.com>
2024-05-07 20:32:39 +0530

    changes commite

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -9,7 +9,7 @@ const Heading = ({ heading }) => {
         initial={{ y: 100 }}
         animate={{ y: 0 }}
         transition={{ duration: 0.5 }}
-        className={`font-hind text-[12vw] uppercase md:leading-[13rem] sm:leading-[6rem] leadimg-[0rem] select-none text-transparent bg-clip-text bg-gradient-to-tl from-btn to-black text-center`}
+        className={`font-hind text-[12vw] uppercase md:leading-[13rem] sm:leading-[6rem] leadimg-[0rem] select-none text-transparent bg-clip-text bg-gradient-to-tl from-btn to-[#7d7d7d] text-center`}
       >
         {heading}
       </motion.p>
