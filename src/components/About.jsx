1760ab93f7e415b00ec5163ec6b2a3076812d798
Mohit <kushwahmohit91@gmail.com>
2024-09-05 14:43:30 +0530

    resume change

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,23 +7,21 @@ const About = () => {
       id="about"
       className="md:px-[6vw] sm:px-[4vw] px-[1rem] min-h-screen relative overflow-hidden grid place-items-center md:overflow-visible overflow-y-scroll no-scrollbar"
     >
-
       <div className="text md:w-3/4  bg-btn sm:px-12 sm:py-6 px-6 py-8 rounded-xl ">
         <p className="text-text text-center mx-auto ">
           Hello! I'm Mohit(Ahmed) Kushwah, A web developer. At the age of 20, I
-          have already gained valuable experience in web development.
-          I am passionate about creating engaging and user-friendly websites.
-          With my skills in web development, I offer a versatile
-          skill set. I am proficient in HTML, CSS, and JavaScript, and I have
-          experience working with frameworks like TailwindCSS, ReactJS and
-          ExpressJS. This allows me to bring ideas to life by seamlessly
-          blending functionality and aesthetics. Continual learning is important
-          to me, as I stay up to date with the latest industry trends and
-          technologies. This ensures that I can leverage the most effective
-          tools and techniques in my projects. I am excited about the
-          opportunity to collaborate on web development projects that allow me
-          to showcase my skills and contribute to the success of businesses and
-          individuals alike.
+          have already gained valuable experience in web development. I am
+          passionate about creating engaging and user-friendly websites. With my
+          skills in web development, I offer a versatile skill set. I am
+          proficient in HTML, CSS, and JavaScript, and I have experience working
+          with frameworks like TailwindCSS, ReactJS and ExpressJS. This allows
+          me to bring ideas to life by seamlessly blending functionality and
+          aesthetics. Continual learning is important to me, as I stay up to
+          date with the latest industry trends and technologies. This ensures
+          that I can leverage the most effective tools and techniques in my
+          projects. I am excited about the opportunity to collaborate on web
+          development projects that allow me to showcase my skills and
+          contribute to the success of businesses and individuals alike.
         </p>
       </div>
       <Heading heading={"about me"} />
